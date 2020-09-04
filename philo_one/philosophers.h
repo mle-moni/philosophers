8921c138d322a65ab69229e0a89508aec2ab04f1
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2020-09-04 13:42:16 +0200

    norminette fix philo_one

--- philo_one/philosophers.h
+++ philo_one/philosophers.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSPHERS_H
-# define PHILOSPHERS_H
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
 # include "utils.h"
 # include <string.h>
@@ -20,8 +20,8 @@
 
 # define DEBUG 0
 
-struct	s_table;
-typedef	uint64_t t_timing;
+struct s_table;
+typedef	uint64_t	t_timing;
 
 typedef struct		s_philo_forks
 {
@@ -68,35 +68,35 @@ typedef struct		s_table
 ** INIT
 */
 
-int			check_parameters(int ac, char **av);
-int			set_parameters(int ac, char **av, t_table *table);
-int			set_philosophers(t_table *table);
-int			mutex_init(t_table *table);
-int			create_monitor(t_philosopher *philo);
-int			create_threads(t_table *table);
-int			setup(t_table *table);
+int					check_parameters(int ac, char **av);
+int					set_parameters(int ac, char **av, t_table *table);
+int					set_philosophers(t_table *table);
+int					mutex_init(t_table *table);
+int					create_monitor(t_philosopher *philo);
+int					create_threads(t_table *table);
+int					setup(t_table *table);
 
 /*
 ** WHILE THE SIMULATION IS RUNNING
 */
 
-void		*philo_routine(void *param);
-void		*monitor_routine(void *param);
+void				*philo_routine(void *param);
+void				*monitor_routine(void *param);
 
 /*
 ** END SIMULATION
 */
 
-int			join_philosophers(t_table *table);
-int			end_simulation(t_table *table);
+int					join_philosophers(t_table *table);
+int					end_simulation(t_table *table);
 
 /*
 ** UTILS
 */
 
-t_timing	get_time(t_timing simulation_start);
-void		sleep_ms(t_timing ms);
-void		status_print(char *status, t_philosopher *philo);
-void		print_parameters(int ac, char **av);
+t_timing			get_time(t_timing simulation_start);
+void				sleep_ms(t_timing ms);
+void				status_print(char *status, t_philosopher *philo);
+void				print_parameters(int ac, char **av);
 
 #endif
